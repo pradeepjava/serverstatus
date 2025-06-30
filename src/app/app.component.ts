41482b9ab879e09de9ddf70c56fc80f5981505cd
Pradeep Kumar <pradeepkumar25aug@gmail.com>
2025-06-30 23:47:36 +0530

    it was missed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
+import { ArticleComponent } from "./dashboard/article/article.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, ArticleComponent],
 })
 export class AppComponent {
   
