8cad09453fb14bca09f664bdfe64b76e74da4463
Pradeep Kumar <pradeepkumar25aug@gmail.com>
2025-07-02 22:42:22 +0530

    attribute type component selector

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { ButtonComponent } from "../../shared/button/button.component";
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, ButtonComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css'
 })
