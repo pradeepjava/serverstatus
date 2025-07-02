4f2344a8377746f7368c74bc37325e7571cf051c
Pradeep Kumar <pradeepkumar25aug@gmail.com>
2025-07-02 22:20:15 +0530

    added new-ticket component and renamed article component into tickets component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { ArticleComponent } from "./dashboard/article/article.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
+import { TicketsComponent } from './dashboard/tickets/tickets.component';
+import { NewTicketComponent } from './dashboard/tickets/new-ticket/new-ticket.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, ArticleComponent],
+  imports: [HeaderComponent, ServerStatusComponent, TrafficComponent, DashboardItemComponent, TicketsComponent],
 })
 export class AppComponent {
   
